a55840271342c5c483b29f963d38f19432b9448e
Vrielle Guevarra <vrielleguevarra@gmail.com>
2026-06-24 16:20:36 -0400

    Added comments clarifying code

--- Car.java
+++ Car.java
@@ -1,5 +1,9 @@
 package vehicle;
 import java.util.*;
+
+// This code is part of an assignment from an Intro to Java course practicing OOP and inheritance.
+// This is a Car object that serves as the parent class for other car objects, such as ElectricCar, GasPoweredCar, etc.
+
 public abstract class Car {
     private String carMake;
     private String carModel;
