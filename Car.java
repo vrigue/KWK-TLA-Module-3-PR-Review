94837cfaa25aa4856a7af69de79ba95a0d31fd11
Vrielle Guevarra <vrielleguevarra@gmail.com>
2026-06-24 16:27:34 -0400

    Added code for review

--- Car.java
+++ Car.java
@@ -5,5 +5,79 @@ import java.util.*;
 // This is a Car object that serves as the parent class for other car objects, such as ElectricCar, GasPoweredCar, etc.
 
 public abstract class Car {
-    
+    private String carMake;
+    private String carModel;
+    private double odometer;
+
+    public Car(String make, String model, double startingMileage){
+        carMake = make;
+        carModel = model;
+
+        if (startingMileage < 0) {
+            throw new IllegalArgumentException("FAILED: The starting mileage CANNOT be negative.");
+        }
+        else {
+            odometer = startingMileage; 
+        }
+    }
+
+    public Car(String make, String model){
+        carMake = make;
+        carModel = model;
+        odometer = 0; 
+    }
+
+    public boolean canDrive(double miles){
+        if (miles < 0){
+            throw new IllegalArgumentException("FAILED: A car cannot drive NEGATIVE miles.");
+        }
+
+        if (getRemainingRange() - miles >= 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }   
+
+    public abstract void drive (double miles);
+
+    public String toString(){
+        return String.format("%s %s (%.1f mi)", getMake(), getModel(), getMileage());
+    }
+
+    public double getMileage(){
+        return odometer;
+    }
+
+    public String getMake(){
+        return carMake;
+    }   
+
+    public String getModel(){
+        return carModel;
+    }
+
+    public abstract double getRemainingRange();
+
+    protected void addMileage(double miles){
+        odometer += miles;
+    }
+
+    public int roadTrip (List<Double> milesEachDay){
+        int days = 0;
+        for (int i = 0; i < milesEachDay.size(); i++){
+            if (milesEachDay.get(i) < 0) {
+                throw new IllegalArgumentException("FAILED: The starting mileage CANNOT be negative.");
+            }
+            else if (canDrive(milesEachDay.get(i))){
+                drive(milesEachDay.get(i));
+                days++;
+            }
+            else {
+                return days;
+            }
+        }
+        return milesEachDay.size();
+    }
 }
